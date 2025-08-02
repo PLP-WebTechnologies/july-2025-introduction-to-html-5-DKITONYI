c3fbf42360f3111566345635a8371199fec4e48a
DKITONYI <kitonyidiana@gmail.com>
2025-08-02 21:25:44 +0300

    Assignment 2

---
+++ calculator.py
@@ -0,0 +1,24 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+operation = input("Choose an operation (+, -, *, /): ")
+
+
+if operation == "+":
+    result = num1 + num2
+    print("Result:", result)
+elif operation == "-":
+    result = num1 - num2
+    print("Result:", result)
+elif operation == "*":
+    result = num1 * num2
+    print("Result:", result)
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid operation. Please choose +, -, *, or /.")
